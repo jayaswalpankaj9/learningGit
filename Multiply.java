32850c2df9349284c2c42da280a24b68bcaa7896
Pankaj Jayaswal <jayaswalpankaj9@gmail.com>
2021-10-26 21:35:07 +0530

    add Multipl.javay

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+import javax.print.event.PrintEvent;
+
+public class Multiply {
+    public static void main(String[] args) {
+        int a = 9, b= 7;
+        int product = a*b;
+
+        System.out.println(product);
+    }
+}
